8405e43b2e065e65a22382006198fbcf565acde5
Vinicius Rodrigues <Vinicius Rodrigues@VINICIUS>
2026-05-20 09:08:10 -0300

    Fix login test valid flow by validating Log out visibility

--- Testes/login_test.js
+++ Testes/login_test.js
@@ -3,16 +3,34 @@ Feature('Login Demo Blaze');
 // Cenário positivo
 Scenario('login com credenciais válidas', async ({ I }) => {
   I.amOnPage('/');
+  I.click("//a[text()='Sign up']");
+  I.waitForVisible('#signInModal', 10);
+
+  const randomUser = 'usuario' + Date.now();
+  I.waitForVisible('#sign-username', 10);
+  I.waitForVisible('#sign-password', 10);
+  I.click('#sign-username');
+  I.fillField('#sign-username', randomUser);
+  I.click('#sign-password');
+  I.fillField('#sign-password', 'senha123');
+  I.seeInField('#sign-username', randomUser);
+  I.seeInField('#sign-password', 'senha123');
+  I.waitForElement("//button[text()='Sign up']", 10);
+  I.click("//button[text()='Sign up']");
+
+  I.seeInPopup('Sign up successful.');
+  I.acceptPopup();
+
   I.waitForElement("//a[text()='Log in']", 10);
   I.click("//a[text()='Log in']");
   I.waitForVisible('#logInModal', 10);
 
-  I.fillField('#loginusername', 'vinicius');
-  I.fillField('#loginpassword', '123456');
+  I.fillField('#loginusername', randomUser);
+  I.fillField('#loginpassword', 'senha123');
   I.click("//button[text()='Log in']");
 
-  I.waitForElement("//a[text()='Log out']", 10);
-  I.see('Log out');
+  I.waitForVisible("//a[text()='Log out']", 10);
+  I.seeElement("//a[text()='Log out']");
 
   console.log('✅ Login válido executado com sucesso!');
 });
